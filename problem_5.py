5a35c77c7836bed5bbaa4a86a14f87d91a1f86a5
suman sorav <112455418+sumansorav123@users.noreply.github.com>
2024-10-21 07:24:04 +0530

    Add files via upload

---
+++ problem_5.py
@@ -0,0 +1,10 @@
+#create a class "pets from a class animal and ferther create  a class "dog" from pets and add a method "blaw to the class"
+
+class Animal:
+    pass
+class Pets(Animal):
+    pass
+class Dog(Pets):
+    @staticmethod
+    def bhow():
+        print("Bhow Bhow!")
